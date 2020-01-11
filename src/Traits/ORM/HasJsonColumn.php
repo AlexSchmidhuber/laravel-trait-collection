9cd433e2480f9669d2d6604d3d391c119ad5a1f9
Alexander Schmidhuber <alexander.schmidhuber@gmail.com>
2020-01-11 21:21:56 +0100

    Added custom json column support

--- src/Traits/ORM/HasJsonColumn.php
+++ src/Traits/ORM/HasJsonColumn.php
@@ -5,8 +5,22 @@ namespace AlexSchmidhuber\LaravelTraitCollection\Traits\ORM;
 trait HasJsonColumn
 {
 
-    public function get($key = null, $column = 'json')
+    public static function initializeHasJsonColumn()
     {
+        $this->fillable[] = $this->setJsonColumn();
+    }
+
+    public function setJsonColumn()
+    {
+        return 'json';
+    }
+
+    public function get($key = null, $column = null)
+    {
+        if(is_null($column)) {
+            $column = $this->setJsonColumn();
+        }
+
         $dataStore = collect(json_decode($this->{$column}));
 
         if (is_null($key)) {
@@ -16,8 +30,12 @@ trait HasJsonColumn
         }
     }
 
-    public function set(array $data, $column = 'json')
+    public function set(array $data, $column = null)
     {
+        if(is_null($column)) {
+            $column = $this->setJsonColumn();
+        }
+
         $dataStore = is_null($this->get(null, $column)) ? [] : $this->get(null, $column)->toArray();
 
         $this->{$column} = json_encode(array_merge($dataStore, $data));
